a59cadc99341aa6076fc3aba06163ab0c34adcd5
Tanvi Mehta <100122388+TanVishal1114@users.noreply.github.com>
2024-02-26 19:43:58 +0000

    Scope done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,9 +6,48 @@ if (true) {
     // console.log("INNER: ", a);
     
 }
-
-
-
-// console.log(a);
+ //console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "tanvi"
+    if (username === "tanvi") {
+        const website = " youtube"
+         console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
