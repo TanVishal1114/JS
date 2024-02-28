f020300bc1917d60b3694a6e77acee6c4cfa7d3f
Tanvi Mehta <100122388+TanVishal1114@users.noreply.github.com>
2024-02-28 21:10:56 +0000

    loop basic done

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -13,3 +13,28 @@ const greetings = "Hello world!"
 for (const greet of greetings) {
     //console.log(`Each char is ${greet}`)
 }
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
